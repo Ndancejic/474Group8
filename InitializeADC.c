7cac56e8f0d5216f1aff55b6674017548d0bc531
Nikola <nikola.dancejic@live.com>
2018-07-12 15:12:14 -0700

    added notes about ADC initialization

--- InitializeADC.c
+++ InitializeADC.c
@@ -5,4 +5,19 @@ void Init_ADC(void){
   GPIO_REG_PORTE |= 0x08; //analog function
   GPIO_DEN_PORTE &= ~0x08; //enable analog
   GPIO_AMSEL_PORTE |= 0x08; //disable isolation
+
+
+  /*
+  enable module
+  diable ss3
+  trigger sequencer 3
+  tale one sample at time set flag at 1st sample
+  enable adc0 interrupt Mask
+
+  Temo = (147.5 - (247.5 * adc0_ssfifo3_r)/4096.0); read result of ADC_CLK_EN
+  handler measure temperature and determines whcih LED to turn once
+  and determines which led to turn on
+  clear flag, clear interrupt
+  ADC0_ISC_R
+  */
 }
